5acd4061fa719a9d74a04f481da8cb86274846a0
Aaron Heidgerken-Greene <aheidgerken@smm.org>
2017-08-11 14:01:57 -0500

    moved pifig to template

--- piFig/src/autostart.js
+++ piFig/src/autostart.js
@@ -3,9 +3,10 @@ obtain(['./src/utils.js', 'child_process'], ({ copyConfigFile, call: Call }, { e
   var mainDir = __dirname.substring(0, __dirname.indexOf('piFig/src'));
   var startup = '@reboot startx ' + mainDir + 'node_modules/electron/dist/electron ' + mainDir;
 
-  console.log(mainDir);
+  console.log(startup);
 
   exports.configure = ()=> {
+    console.log(startup);
     var command = '(crontab -u pi -l ; echo "' + startup + '") | crontab -u pi -';
     if (__dirname.indexOf('/home/pi') >= 0) exec(command);
     else console.error('System not a pi, preventing install');

a96fdcd372ff228380f3259322f8c08b2eec4929
Aaron Heidgerken-Greene <aheidgerken@smm.org>
2017-08-11 14:22:52 -0500

    moved pifig to template

--- piFig/src/autostart.js
+++ piFig/src/autostart.js
@@ -1,20 +1,20 @@
 obtain(['./src/utils.js', 'child_process'], ({ copyConfigFile, call: Call }, { exec })=> {
 
   var mainDir = __dirname.substring(0, __dirname.indexOf('piFig/src'));
-  var startup = '@reboot startx ' + mainDir + 'node_modules/electron/dist/electron ' + mainDir;
+  var startup = 'sudo startx ' + mainDir + 'node_modules/electron/dist/electron ' + mainDir;
 
   console.log(startup);
 
-  exports.configure = ()=> {
-    console.log(startup);
-    var command = '(crontab -u pi -l ; echo "' + startup + '") | crontab -u pi -';
-    if (__dirname.indexOf('/home/pi') >= 0) exec(command);
-    else console.error('System not a pi, preventing install');
-  };
-
   exports.remove = ()=> {
-    var command = 'crontab -u pi -l | grep -v "' + startup + '"  | crontab -u pi -';
+    var command = 'sed -i".bak" "/node_modules\/.bin\/electron/d" /home/pi/.bashrc > /home/pi/.bashrc';
     if (__dirname.indexOf('/home/pi') >= 0) exec(command);
     else console.error('System not a pi, preventing uninstall');
+  };
+
+  exports.configure = ()=> {
+    exports.remove();
+    var command = 'echo "' + startup + '" >> /home/pi/.bashrc';
+    if (__dirname.indexOf('/home/pi') >= 0) exec(command);
+    else console.error('System not a pi, preventing install');
   };
 });

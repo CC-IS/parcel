36e382de08cba9e38ab40925cbf9b70e48c20315
Aaron Heidgerken-Greene <aheidgerken@smm.org>
2018-05-10 13:16:43 -0500

    updating install

--- ForBoot/install.sh
+++ ForBoot/install.sh
@@ -7,7 +7,9 @@ echo -e "\nInstalling node:"
 
 curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
 
-sudo apt-get install --no-install-recommends build-essential hostapd dnsmasq network-manager xserver-xorg xinit xserver-xorg-video-fbdev xserver-xorg-video-fbturbo libxss1 libgconf-2-4 libnss3 git nodejs libgtk2.0-0 libxtst6
+sudo apt-get install xserver-xorg-video-fbturbo
+
+sudo apt-get install --no-install-recommends build-essential hostapd dnsmasq network-manager xserver-xorg xinit xserver-xorg-video-fbdev libxss1 libgconf-2-4 libnss3 git nodejs libgtk2.0-0 libxtst6
 
 echo  -e "\nClone the application"
 

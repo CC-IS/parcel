885c9642e00c2d5008d9ef12c4b4adf05404072f
Aaron Heidgerken-Greene <aheidgerken@smm.org>
2018-09-20 13:07:27 -0500

    first parcel commit

--- ForBoot/setup/install.sh
+++ ForBoot/setup/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-REPO_NAME=Electron_Template
+REPO_NAME=MuseAppTemplate
 ACCOUNT=heidgera
 
 echo -e "\nInstalling node:"
@@ -15,17 +15,15 @@ sudo apt-get --assume-yes install --no-install-recommends build-essential hostap
 
 sudo apt-get --assume-yes install libasound2
 
+echo  -e "\nClone the wrapper"
+
+git clone --recurse-submodules https://github.com/heidgera/Parcel.git
+
+cd Parcel
+
 echo  -e "\nClone the application"
 
-git clone https://github.com/${ACCOUNT}/${REPO_NAME}.git
-
-cd ${REPO_NAME}
-
-echo  -e "\nInit the submodules:"
-
-git submodule init
-
-git submodule update
+git clone --recurse-submodules https://github.com/${ACCOUNT}/${REPO_NAME}
 
 echo  -e "\nInstalling dependencies for application:"
 
--- startApp.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-startx /home/pi/Mission4/node_modules/electron/dist/electron /home/pi/Mission4

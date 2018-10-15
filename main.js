ee437bbd6ca567103d976d0c8b6891d68c8ac980
Aaron Heidgerken-Greene <aheidgerken@smm.org>
2018-10-15 11:57:43 -0500

    finalizing muse remove

--- main.js
+++ main.js
@@ -1,8 +1,20 @@
 'use strict';
 
-require('./common/src/muse/main.js');
-
 const electron = require('electron');
+
+global.obtain = (addr, func)=> {
+  var _this = this;
+  var objs = [];
+  if (addr.length <= 0) func();
+  else {
+    addr.forEach(function (adr, ind, arr) {
+      objs[ind] = require(adr);
+    });
+
+    func.apply(null, objs);
+  };
+
+};
 
 var ipcMain = electron.ipcMain;
 

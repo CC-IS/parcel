431fab97f05b925362e13126b3aac7e33be277bd
Aaron Heidgerken-Greene <aheidgerken@smm.org>
2018-10-15 11:00:21 -0500

    removed muse dependency from newMonitor code

--- ForBoot/appData/config.js
+++ ForBoot/appData/config.js
@@ -2,13 +2,13 @@ module.exports = {
   appRepo: 'https://github.com/heidgera/midiLightMirror',
   showDevTools: true,
   preventStartup: false,
-  // windows: [
-  //   {
-  //     label: 'main',
-  //     fullscreen: false,
-  //     alwaysOnTop: false,
-  //     //displayId: '69733248', //manually specify
-  //     file: 'app/local/index.html',
-  //   },
-  // ],
+  windows: [
+    {
+      label: 'main',
+      fullscreen: false,
+      alwaysOnTop: false,
+      //displayId: '69733248', //manually specify
+      file: 'app/local/index.html',
+    },
+  ],
 };
--- newMonitor.js
+++ newMonitor.js
@@ -1,67 +1,72 @@
 'use strict';
 
-var remote = require('electron').remote;
+var { remote, ipcRenderer: comm } = require('electron');
 
 var config = remote.getGlobal('config');
 
 var process = remote.process;
 
-var obtains = [
-  'µ/components/',
-  'electron',
-];
+var µ = query=>document.querySelector(query);
 
-obtain(obtains, (comps, { ipcRenderer: comm })=> {
+/*if (document) window.µ = function (id, elem) {
+  var ret;
+  var root = ((elem) ? elem : document);
+  var spl = id.split('>');
+  switch (spl[0].charAt(0)) {
+    case '|':
+      ret = root;
+      break;
+    case '+':
+      ret = document.createElement(spl[0].substring(1));
+      if (elem) elem.appendChild(ret);
+      break;
+    case '#':
+      ret = root.querySelector(spl[0]);
+      break;
+    default:
+      ret = Array.from(root.querySelectorAll(spl[0]));
 
-  exports.app = {};
+      ret.style = function (mem, val) {
+        for (let i = 0; i < ret.length; i++) {
+          ret[i].style[mem] = val;
+        }
+      };
 
-  exports.app.start = ()=> {
+      //}
+      break;
+  }
+  if (spl.length <= 1) return ret;
+  else return ret.getAttribute(spl[1]);
+};*/
 
-    // comm.send('interwindow', {
-    //   target: 'playback',
-    //   channel: 'video',
-    //   data: {
-    //     url: lastURL,
-    //     length: recordTime,
-    //   },
-    // });
+document.addEventListener('DOMContentLoaded', function (event) {
+  console.log('here');
+  var windows = null;
 
-    var windows = null;
+  windows = config.windows;
+  config.windows.forEach(wind=> {
+    let newOpt = document.createElement('option');
+    newOpt.textContent = wind.label;
+    newOpt.value = wind.label;
+    µ('#windows').appendChild(newOpt);
+  });
 
-    windows = config.windows;
-    config.windows.forEach(wind=> {
-      let newOpt = µ('+drop-opt', µ('#windows'));
-      newOpt.textContent = wind.label;
-      newOpt.value = wind.label;
-    });
-
-    µ('#windows').onSelect = (node, index)=> {
-      µ('#demo').src = windows[index].file;
-    };
-
-    µ('#save').onclick = ()=> {
-      if (µ('#windows').value) {
-        comm.send('window-select', {
-          window: µ('#windows').value,
-        });
-      } else {
-        µ('#growl').message('Please select a window', 'warn');
-      }
-    };
-
-    document.onkeypress = (e)=> {
-    };
-
-    document.onkeyup = (e)=> {
-      if (e.which == 73 && e.getModifierState('Control') &&  e.getModifierState('Shift')) {
-        remote.getCurrentWindow().toggleDevTools();
-      }
-    };
-
-    process.on('SIGINT', ()=> {
-      process.nextTick(function () { process.exit(0); });
-    });
+  µ('#windows').onchange = ()=> {
+    var wnd = config.windows.find(wind=>wind.label == µ('#windows').value);
+    µ('#demo').src = wnd.file;
   };
 
-  provide(exports);
+  µ('#save').onmousedown = ()=> {
+    if (µ('#windows').value) {
+      comm.send('window-select', {
+        window: µ('#windows').value,
+      });
+    } else {
+      //µ('#growl').message('Please select a window', 'warn');
+    }
+  };
+
+  process.on('SIGINT', ()=> {
+    process.nextTick(function () { process.exit(0); });
+  });
 });

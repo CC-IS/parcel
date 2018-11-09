4372e940646129beb521872034ae8deac9e8aa1d
Aaron Heidgerken-Greene <aheidgerken@smm.org>
2018-11-09 10:03:26 -0600

    added wired router config

--- newMonitor.js
+++ newMonitor.js
@@ -8,37 +8,6 @@ var process = remote.process;
 
 var µ = query=>document.querySelector(query);
 
-/*if (document) window.µ = function (id, elem) {
-  var ret;
-  var root = ((elem) ? elem : document);
-  var spl = id.split('>');
-  switch (spl[0].charAt(0)) {
-    case '|':
-      ret = root;
-      break;
-    case '+':
-      ret = document.createElement(spl[0].substring(1));
-      if (elem) elem.appendChild(ret);
-      break;
-    case '#':
-      ret = root.querySelector(spl[0]);
-      break;
-    default:
-      ret = Array.from(root.querySelectorAll(spl[0]));
-
-      ret.style = function (mem, val) {
-        for (let i = 0; i < ret.length; i++) {
-          ret[i].style[mem] = val;
-        }
-      };
-
-      //}
-      break;
-  }
-  if (spl.length <= 1) return ret;
-  else return ret.getAttribute(spl[1]);
-};*/
-
 document.addEventListener('DOMContentLoaded', function (event) {
   console.log('here');
   var windows = null;

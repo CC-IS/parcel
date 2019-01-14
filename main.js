5c82b73e924168874c9aba9f5849ea0aaea3d4a2
Aaron Heidgerken-Greene <aheidgerken@smm.org>
2019-01-14 13:27:33 -0600

    changes for main obtain

--- main.js
+++ main.js
@@ -3,16 +3,8 @@
 const electron = require('electron');
 
 global.obtain = (addr, func)=> {
-  var _this = this;
-  var objs = [];
   if (addr.length <= 0) func();
-  else {
-    addr.forEach(function (adr, ind, arr) {
-      objs[ind] = require(adr);
-    });
-
-    func.apply(null, objs);
-  };
+  else func.apply(null, addr.map(adr=>require(adr)));
 
 };
 
